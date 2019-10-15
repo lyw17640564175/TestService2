993f3a0ac5ffeb0aba5429e775f13bd1db733b94
enwn <280043932@qq.com>
2019-10-15 16:27:14 +0800

    sb

--- app/src/main/java/com/example/testservice2/MainActivity.java
+++ app/src/main/java/com/example/testservice2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.testservice2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        Log.i("tag", "onCreate: "+"kskkssk");
     }
 
     private void initView() {
